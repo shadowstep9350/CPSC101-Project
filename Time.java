989ab631896086c254dbf06794e3cfe2c5f75aab
shadowstep9350 <thomastomahawk@msn.com>
2016-03-11 12:59:39 -0800

    Update Time.java

--- Time.java
+++ Time.java
@@ -2,7 +2,7 @@
 * Class that makes sure that the class time and duration make sense and creates an object with the times as variables.
 *
 * @author Duncan Render
-* @version 0.01
+* @version 1.01
 */
 package cpsc101.highoctane.model;
 import java.text.*;
@@ -11,15 +11,16 @@ public class Time
 {
 	private static DecimalFormat cf = new DecimalFormat("00,00"); //Lets us take numbers and display them as "hours:minutes".
 	private static DecimalFormatSymbols symbols = cf.getDecimalFormatSymbols();
-	private int START_TIME; //The time that the class starts. Kind of self-explanatory...
+	private int Start_Time; //The time that the class starts. Kind of self-explanatory...
 	private int Start_Hours; //The hour that the class starts at. 
 	private int Start_Minutes; //The minute that the class starts at. This needs to be separate from hours at first because of csv format.
 	private int duration; //The duration of the class.
 	private int Duration_Minutes;
 	private int Duration_Hours;
 	private int End_Time; //Time that class ends.
-	private String MEET_TIME; //We can use the decimal format to make the start time into a string.
-	private String END_OF_CLASS; //Makes a string for the time that class ends, in case we want to show it somewhere.
+	private String Meet_Time; //We can use the decimal format to make the start time into a string.
+	private String End_of_Class; //Makes a string for the time that class ends, in case we want to show it somewhere.
+	private boolean created = false;
 	
 	/**
 	* The constructor for the class object.
@@ -35,7 +36,7 @@ public class Time
 	*
 	* Checks to see if the start and end of class is between 08:30 and 21:00. If not, it throws an exception.
 	* Also I tried to make sure that you couldn't start the class before 21:00 and end it after 08:30. 
-	* I haven't tested that particular case yet but it should work.
+	* I have tested this particular case and it does work.
 	*
 	* Also does a bit of math to make sure the hour is between 0 and 23 and the minute is between 0 and 59.
 	* It shouldn't actually ever change the number if the csv file is correct, but it's just there as a safeguard.
@@ -51,24 +52,26 @@ public class Time
 		setStart();
 		setEnd();
 		setMeetTime();
-		setEndOfClass();
+		setEndofClass();
 		validTimes();
+		created = true;
 	}
 	
 	//Here are the standard getters.
 	public String getTime()
 	{
-		return MEET_TIME;
+		return Meet_Time;
 	}
 	
 	public int getDuration()
 	{
+		setDuration();
 		return duration;
 	}
 	
 	public int getRawTime()
 	{
-		return START_TIME;
+		return Start_Time;
 	}
 	
 	public int getRawEnd()
@@ -78,7 +81,7 @@ public class Time
 	
 	public String getClassEnd()
 	{
-		return END_OF_CLASS;
+		return End_of_Class;
 	}
 	
 	//And here are the setters
@@ -91,6 +94,14 @@ public class Time
 	public void setHours(int hours) //sets the hours for the start of class and checks to make sure the times are still valid.
 	{
 		Start_Hours = hours%24;
+		if(created) 
+		{
+			setStart();
+			setEnd();
+			setMeetTime();
+			setEndofClass();
+			validTimes();
+		}
 	}
 	
 	
@@ -103,11 +114,22 @@ public class Time
 	public void setDurationHours(int hours)
 	{
 		Duration_Hours = hours%24;
+		if(created) 
+		{
+			setEnd();
+			setEndofClass();
+			validTimes();
+		}
+	}
+	
+	private void setDuration()
+	{
+		duration = Duration_Hours*100 + Duration_Minutes;
 	}
 	
 	private void setStart()
 	{
-		START_TIME = Start_Hours*100 + Start_Minutes;
+		Start_Time = Start_Hours*100 + Start_Minutes;
 	}
 	
 	private void setEnd()
@@ -117,12 +139,12 @@ public class Time
 	
 	private void setMeetTime()
 	{
-		MEET_TIME = cf.format(START_TIME);
+		Meet_Time = cf.format(Start_Time);
 	}	
 	
-	private void setEndOfClass()
+	private void setEndofClass()
 	{
-		END_OF_CLASS = cf.format(End_Time);
+		End_of_Class = cf.format(End_Time);
 	}
 	
 	/**
@@ -130,12 +152,12 @@ public class Time
 	*
 	* I added the exception because Ryley did that in his course id. I don't know if you guys want it like that though?
 	*/
-	public void validTimes() throws IllegalArgumentException
+	private void validTimes() throws IllegalArgumentException
 	{
 		
-		if (START_TIME < 830 || START_TIME >= 2100 || End_Time > 2100 || End_Time <= 830 || End_Time <= START_TIME)
+		if (Start_Time < 830 || Start_Time >= 2100 || End_Time > 2100 || End_Time <= 830 || End_Time <= Start_Time)
 		{ 
-			throw new IllegalArgumentException("The course times (" + MEET_TIME + ", " + END_OF_CLASS + ") do no fit between 08:30 and 21:00");
+			throw new IllegalArgumentException("The course times (" + Meet_Time + ", " + End_of_Class + ") do no fit between 08:30 and 21:00");
 		}		
 		return;
 	}
@@ -147,7 +169,7 @@ public class Time
 	*/
 	public boolean overlapTime(Time other) 
 	{
-		if ((other.getRawTime() >= START_TIME && other.getRawTime() <= End_Time) || (START_TIME >= other.getRawTime() && START_TIME <= other.getRawEnd()))
+		if ((other.getRawTime() >= Start_Time && other.getRawTime() <= End_Time) || (Start_Time >= other.getRawTime() && Start_Time <= other.getRawEnd()))
 		{
 			return true;
 		}
